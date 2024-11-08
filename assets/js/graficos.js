0ad7bacf38b1abb76417daab1e40bb2f84561e47
Angelo Rafael <113860124+RafaSicron@users.noreply.github.com>
2024-11-07 23:15:55 -0300

    Primeira parte das modifcações finais

---
+++ assets/js/graficos.js
@@ -0,0 +1,30 @@
+
+const slides = document.querySelectorAll('.graphsCard')
+const leftButton = document.getElementById('leftButton')
+const rightButton = document.getElementById('rightButton')
+
+let counter = 0
+
+slides.forEach((element, index) => {
+    element.style.left = `${index * 101}%`
+})
+
+const slideImage = () => {
+    slides.forEach((element) => element.style.transform = `translateX(-${counter * 101}%)`)
+}
+
+
+leftButton.addEventListener('click', () => {
+    if(counter > 0) {
+        counter--
+        slideImage()
+    }
+  
+})
+
+rightButton.addEventListener('click', () => {
+    if(counter < slides.length - 1) {
+        counter++
+        slideImage()
+    }
+})
