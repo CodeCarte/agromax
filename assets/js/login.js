18ae16b7545b06df3af1b1dcaa10dc2d690563f3
Angelo Rafael <113860124+RafaSicron@users.noreply.github.com>
2024-11-09 22:06:46 -0300

    Modificação final do projeto

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,5 +1,10 @@
+const loginForm = document.getElementById('login-form')
 const senha = document.getElementById("senha");
 const icon = document.getElementById("icon");
+const emailAdm = 'admin@hotmail.com'
+const passwordAdm = 'admin'
+const emailInput = document.getElementById('usuario')
+const passwordInput = document.getElementById('senha')
 
 function showHide(){
     if(senha.type === "password"){
@@ -10,3 +15,15 @@ function showHide(){
     icon.classList.remove('hide') 
     }
 }
+
+loginForm.addEventListener('submit', (ev) => {
+    if(emailInput.value !== emailAdm && passwordInput !== passwordAdm) {
+        alert("Email ou senha de admin incorretas:\n\nAdmin: admin@hotmail.com\nSenha: admin");
+        confirm('Deseja preencher automaticamente?')
+        if(confirm) {
+            emailInput.value = emailAdm
+            passwordInput.value = passwordAdm
+        }
+        ev.preventDefault()
+    }
+})
